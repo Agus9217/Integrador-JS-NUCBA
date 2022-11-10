8e36e091e55e8c5e1a7b088bd0eef164b65dff64
Agus9217 <justinibarrola92@gmail.com>
2022-11-10 14:38:25 -0300

    integraddor-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
 const API_URL = 'https://api.dailymotion.com/videos?channel=sport&limit=50&flags=verified'
 
 const apiFetchReq = async () => {
-    const res = await fetch(API_URL)
-    const data = await res.json()
-    filterList(data)
+  const res = await fetch(API_URL)
+  const data = await res.json()
+  sliceList(data.list)
 }
 
-const filterList = (data) => {
-    const result = data.list
-    result.map(item => console.log(item))
+function sliceList(data) {
+  const result = data.slice(0, 10)
+  filterObj(result)
 }
 
-apiFetchReq()
+const filterObj = (data) => {
+  console.log(data)
+}
+
+
+
+
+
+
+const init = () => {
+  apiFetchReq()
+}
+
+init()
