0064b8ef2b9d1bf538811f7a74578ed1a9a73f97
Agus9217 <justinibarrola92@gmail.com>
2022-11-11 16:42:18 -0300

    integraddor-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const API_URL = 'https://api.dailymotion.com/videos?channel=sport&limit=50&flags=verified'
 
+const trendindNews = document.getElementById('trendingNews')
+
 const apiFetchReq = async () => {
   const res = await fetch(API_URL)
   const data = await res.json()
@@ -7,21 +9,35 @@ const apiFetchReq = async () => {
 }
 
 function sliceList(data) {
-  const result = data.slice(0, 10)
+  const result = data.slice(0, 5)
   filterObj(result)
 }
 
 const filterObj = (data) => {
-  console.log(data)
+  data.map(item => {
+    renderTrending(item)
+  })
+}
+
+const renderTrending = (data) => {
+ const createDiv = document.createElement('div')
+ createDiv.innerHTML = `
+ <img src="http://www.dailymotion.com/thumbnail/video/${data.id}" alt="">
+ <div class="trendingGroup">
+   <div>
+     <span class="trendingCategoryDate">Entertainment / 3 years ago</span>
+     <span class="trendingInfo">${data.title}</span>
+   </div>
+ </div>
+ `  
+ trendindNews.appendChild(createDiv)
 }
 
 
-
-
-
-
 const init = () => {
-  apiFetchReq()
+  window.addEventListener('DOMContentLoaded', () => {
+    apiFetchReq()
+  })
 }
 
 init()
